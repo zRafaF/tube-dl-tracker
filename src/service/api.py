0ea11aba914a3673d4f0b27d32e95cdef883018b
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2024-01-28 02:08:20 -0300

    Added routing

--- src/service/api.py
+++ src/service/api.py
@@ -9,9 +9,22 @@ from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 
+GLOBALS = {
+    "pages": [{
+        'name': 'home',
+        'url': '/'
+    }, {
+        'name': 'items',
+        'url': 'items/1'
+    }]
+}
+
 app_fastapi = FastAPI()
 app_fastapi.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
+
+templates.env.globals["globals"] = GLOBALS
+
 
 session = app_rocketry.session
 
@@ -27,12 +40,12 @@ async def get_tasks():
 @app_fastapi.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse(
-        request=request, name="home.j2", context={"id": id}
+        request=request, name="home.j2", context={"current_page": "home"}
     )
 
 
 @app_fastapi.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
+async def read_items(request: Request, id: str):
     return templates.TemplateResponse(
-        request=request, name="item.j2", context={"id": id}
+        request=request, name="items.j2", context={"current_page": "items", "id": id}
     )

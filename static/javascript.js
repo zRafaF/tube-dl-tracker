1ef237469d4613088777c39d8629f47e47050468
zRafaF <zRafaF@users.noreply.github.com>
2024-01-28 14:23:30 +0000

    deploy: c6a039e54e1872c61842f62efbbf341e6d186c8d

---
+++ static/javascript.js
@@ -0,0 +1,7 @@
+// Copyright (c) 2024 Rafael F. Meneses
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+const messageToast = document.getElementById('messageToast');
+new bootstrap.Toast(messageToast).show();
